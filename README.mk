5df2f922909e71cae547120c08c68b51a42bdcf7
pedroreislima <gaffetwo@gmail.com>
2025-05-30 17:00:19 -0300

    developing the first version of the data generating process

--- README.mk
+++ README.mk
@@ -1,3 +1,3 @@
 # dash-domestic
-Dash-domestic is a project made for keep track of domestic expenses.
+Dash-domestic is a project made for keeping track of domestic expenses.
 It uses mysql as a database and is written in python, assisted by pandas and dash libraries.
