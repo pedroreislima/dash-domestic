195918e6fefbc2fdad6dcc9a4645e96738f6a0ac
pedroreislima <gaffetwo@gmail.com>
2025-06-09 11:32:15 -0300

    new file structure

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import datetime
+
+def get_weekdays(year:int, month:int, week_day:int):
+    '''
+    Get all specific days of week (ex. monday) from certain month and year.
+    
+    Return:
+        list of dates on certain month
+    '''
+    month_start = datetime.date(year,month,1)
+    start_date = month_start
+    week_delta = week_day - month_start.weekday()
+    if week_delta >= 0:
+        start_date = month_start + datetime.timedelta(days=week_delta)
+    if week_delta <0:
+        start_date = month_start + datetime.timedelta(days=week_delta + 7)
+        
+    # now we have the first date for that weekday, lets iterate until we are out of bounds on the month.
+    iterate_date = start_date
+    weekday_list = []
+    while month_start.month == iterate_date.month:
+        weekday_list.append(iterate_date)
+        iterate_date = iterate_date + datetime.timedelta(days= 7)
+        print(iterate_date)
+    return weekday_list
